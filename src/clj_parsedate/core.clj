736396862b875de550025458a9a55bc9a13ac425
Eric Normand <eric@skrivapa.se>
2011-06-27 18:52:14 -0300

    First repo

---
+++ src/clj_parsedate/core.clj
@@ -0,0 +1,1 @@
+(ns clj-parsedate.core)
---
+++ test/clj_parsedate/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-parsedate.test.core
+  (:use [clj-parsedate.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))

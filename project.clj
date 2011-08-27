83df8802d79a52447241a436937ae0ea26ce157f
Eric Normand <ericwnormand@gmail.com>
2011-08-27 12:37:11 -0300

    new version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject sphynx "0.2.0"
+(defproject sphynx "0.2.1"
   :description "Tap into the mystery of the Sphynx to parse information from strings."
 
   :dev-dependencies [[swank-clojure        "1.3.2"]
                      [lein-marginalia      "0.6.0"]]
 
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [squarepeg           "0.6.0"]
+                 [squarepeg           "0.6.1"]
                  [joda-time           "1.6.2"]])
